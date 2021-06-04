44e8e9d5e087e515ebb47ace8eec7f8e6b9124f6
lssdeveloper <leandro.serra.10@gmail.com>
2021-06-04 20:48:49 -0300

    Exercício 6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.

--- EstruturaSequencial/exercicio_6.py
+++ EstruturaSequencial/exercicio_6.py
@@ -1,8 +1,26 @@
 """
 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 """
-
+"""
 raio = float(input('Raio ?'))
 pi = 3.14
 area = pi * (raio * raio)
 print('\nA área do círculo é : %.2f' % (area) )
+"""
+
+"""
+Determine a área de um segmento circular com ângulo central de 32º e raio medindo 2 m.
+tt = 3.14 # PI
+r = raio do círculo
+o = medida do ângulo central
+x = área do arco
+"""
+
+tt = 3.14
+r = float(input('Raio ?'))
+o = float(input('Ângulo ?'))
+x = (o * tt * (r * r)) / 360
+print('\nA área do segmento circular possui aproximadamente %.2f m2' % x)
+
+
+
