5ef82de2b75078920cebba539c09fedd80c02a9b
lssdeveloper <leandro.serra.10@gmail.com>
2021-06-04 19:07:11 -0300

    Exercício 5 - Faça um programa que converta metros em centímetros

---
+++ EstruturaSequencial/exercicio_5.py
@@ -0,0 +1,11 @@
+"""
+Faça um programa que converta metros em centímetros
+"""
+# 1 metro = 100 cm
+
+print('Conversor de metro para centímetros !')
+metro = float(input('Informe quantos metros:'))
+
+centimetros = metro * 100
+
+print('%s metro(s) é igual a %d centímetros.' % (metro, centimetros))
